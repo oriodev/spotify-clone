89a13e839063bf890615debc9e8ddc1c3d7fdbbb
ori <93719767+oriodev@users.noreply.github.com>
2024-02-05 19:34:55 +0000

    finished auth

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -4,10 +4,15 @@ import {
   useSessionContext,
   useSupabaseClient,
 } from '@supabase/auth-helpers-react';
-import Modal from './Modal';
-import { useRouter } from 'next/navigation';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+import useAuthModal from '@/hooks/useAuthModal';
+
+import Modal from './Modal';
 
 const AuthModal = () => {
   // returns shit from the supabase provider
@@ -15,17 +20,52 @@ const AuthModal = () => {
   const router = useRouter();
   const session = useSessionContext();
 
+  // set object to our custom hook
+  // that returns an object of isOpen/isClosed setting.
+  const { onClose, isOpen } = useAuthModal();
+
+  // if we click the close btn, set auth modal to closed.
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
+
+  // close modal after login.
+  useEffect(() => {
+    if (session) {
+      // refresh the current route
+      router.refresh();
+      onClose();
+    }
+  }, [session, router, onClose]);
+
   return (
     <Modal
       title="welcome back!"
       description="login to your account"
-      isOpen
-      onChange={() => {}}
+      isOpen={isOpen}
+      onChange={onChange}
     >
       <Auth
+        // overall light/dark theme
+        theme="dark"
+        // who u can log in with, eg. google, github, discord.
+        providers={['github']}
+        // attaches auth model to ur supabase client.
         supabaseClient={supabaseClient}
+        // so u can log in with a link to ur email account.
+        magicLink
         appearance={{
           theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: '#404040',
+                brandAccent: '#22c55e',
+              },
+            },
+          },
         }}
       />
     </Modal>
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,11 @@ import { HiHome } from 'react-icons/hi';
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
 import { twMerge } from 'tailwind-merge';
 import Button from './Button';
+import useAuthModal from '@/hooks/useAuthModal';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useUser } from '@/hooks/useUser';
+import { FaUserAlt } from 'react-icons/fa';
+import toast from 'react-hot-toast';
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -14,9 +19,25 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  // sets auth modal to open
+  const authModal = useAuthModal();
   const router = useRouter();
 
-  const handleLogout = () => {};
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+
+    // todo: reset any playing songs in the future.
+    router.refresh();
+
+    if (error) {
+      toast.error(error.message);
+    } else {
+      toast.success('logged out!');
+    }
+  };
 
   return (
     <div
@@ -56,23 +77,44 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           </button>
         </div>
 
-        {/*  */}
+        {/* so this is the buttons section */}
         <div className="flex justify-between items-center gap-x-4">
-          <>
-            <div>
-              <Button
-                onClick={() => {}}
-                className="bg-transparent text-neutral-300 font-medium"
-              >
-                Sign Up
-              </Button>
-            </div>
-            <div>
-              <Button onClick={() => {}} className="bg-white px-6 py-2">
-                Log In
-              </Button>
-            </div>
-          </>
+          {user ? (
+            <>
+              <div className="flex gap-x-4 items-center">
+                <Button onClick={handleLogout} className="bg-white px-6 py-2">
+                  Log Out
+                </Button>
+              </div>
+              <div>
+                <Button
+                  onClick={() => router.push('/account')}
+                  className="bg-white"
+                >
+                  <FaUserAlt />
+                </Button>
+              </div>
+            </>
+          ) : (
+            <>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="bg-transparent text-neutral-300 font-medium"
+                >
+                  Sign Up
+                </Button>
+              </div>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="bg-white px-6 py-2"
+                >
+                  Log In
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {children}
